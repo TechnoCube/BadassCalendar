405ec4e597279632128457feba94c472dee6aaf9
jonathanfils-aime <jxf131930@utdallas.edu>
2015-10-15 15:11:16 -0500

    basic UI update. -top menu button   -month   -week   -day -redesigned floating button

--- app/src/main/java/groupproject/utd/badasscalendar/MainActivity.java
+++ app/src/main/java/groupproject/utd/badasscalendar/MainActivity.java
@@ -2,12 +2,11 @@ package groupproject.utd.badasscalendar;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,12 +21,6 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-
-                int jo = 10;
-                int pop = 20;
-                String jonathan = "is the main";
 
             }
         });
---
+++ app/src/main/java/groupproject/utd/badasscalendar/newEventFragment.java
@@ -0,0 +1,22 @@
+package groupproject.utd.badasscalendar;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class newEventFragment extends Fragment {
+
+    public newEventFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_new_event, container, false);
+    }
+}
