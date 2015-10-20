a147536f28430ad693bd9b458bc2be8c951d827a
Jay Jensen <jxj125930@utdallas.edu>
2015-10-20 13:06:06 -0500

    Lorem Ipsum

--- app/src/main/java/groupproject/utd/badasscalendar/MainActivity.java
+++ app/src/main/java/groupproject/utd/badasscalendar/MainActivity.java
@@ -24,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
-
+                string latin = "Lorem Ipsum";
                 int jo = 10;
                 int pop = 20;
                 String jonathan = "is the main";
