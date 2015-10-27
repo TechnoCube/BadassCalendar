39a7b67680150473f0969e70129413277bc5ca98
Henry Kim <henrykimss85@gmail.com>
2015-10-27 04:30:35 -0600

    Merge branch 'master' of https://github.com/TechnoCube/BadassCalendar into Henry_Branch
    # Conflicts:
    #	app/src/main/java/groupproject/utd/badasscalendar/MainActivity.java
    Henry: worked on the weeklyView of the program
    -Oct 25 Finished making the weekly format
    -Oct 26 - Working on button

--- app/src/main/java/groupproject/utd/badasscalendar/MainActivity.java
+++ app/src/main/java/groupproject/utd/badasscalendar/MainActivity.java
@@ -1,6 +1,7 @@
 package groupproject.utd.badasscalendar;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -24,12 +25,13 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-
+                //Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                //       .setAction("Action", null).show();
+                startActivity(new Intent(MainActivity.this,MonthlyView.class));
 
             }
         });
--- app/src/main/java/groupproject/utd/badasscalendar/WeeklyView.java
+++ app/src/main/java/groupproject/utd/badasscalendar/WeeklyView.java
@@ -1,5 +1,11 @@
 package groupproject.utd.badasscalendar;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.AdapterView;
+import android.widget.CalendarView;
+import android.widget.Spinner;
+import android.widget.TextView;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -10,9 +16,16 @@ import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 import android.widget.RelativeLayout;
 import android.widget.Button;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 
-public class WeeklyView extends AppCompatActivity {
+public class WeeklyView extends Activity implements AdapterView.OnItemSelectedListener{
+
+    //Drop down list
+    Spinner dropDownlist;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,19 +33,28 @@ public class WeeklyView extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weekly_view);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        dropDownlist = (Spinner) findViewById(R.id.Pull_down);
+        ArrayAdapter adapter = ArrayAdapter.createFromResource(this, R.array.dropDownlist, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        dropDownlist.setAdapter(adapter);
+
 
     }
 
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        TextView myText = (TextView) view; 
+        Toast.makeText(this, "You Selected", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
     public void sunButton(View v){
-    Button button = (Button) v;
+        Button button = (Button) v;
         ((Button) v).setText("click");
     }
 }
+
+
