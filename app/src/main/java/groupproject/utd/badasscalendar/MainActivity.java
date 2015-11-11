5168aa4da7fb05736f758fe7a6371eff6983a44d
Daniel <dgj130030@utdallas.edu>
2015-11-11 13:05:32 -0600

    -created daily view -shows hours of the day, add event button, and can changes different views

--- app/src/main/java/groupproject/utd/badasscalendar/MainActivity.java
+++ app/src/main/java/groupproject/utd/badasscalendar/MainActivity.java
@@ -4,12 +4,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.provider.CalendarContract;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -32,8 +31,6 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-
-
     }
 
     @Override
